7bc73747072fafd22156ad59bb562d94adc2f01a
kim-degracia <69057610+kim-degracia@users.noreply.github.com>
2020-09-13 21:23:32 -0400

    Added Day and Time

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-//Week 5 Homework Code
-//Functions
+function getDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
+}
 
-function showWeather(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#current-weather").innerHTML = `${Math.round(
-    response.data.main.temp
-  )} °C`;
-  document.querySelector(
-    "#humid"
-  ).innerHTML = `Humidity: ${response.data.main.humidity} %`;
-  document.querySelector("#wind").innerHTML = `Wind: ${Math.round(
-    response.data.wind.speed
-  )} km/h`;
-  let h4 = document.querySelector("h4");
-  h4.innerHTML = `${response.data.weather[0].description}`;
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    mintes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -28,8 +43,6 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
-//Bonus
-
 function searchLocation(position) {
   let apiKey = "12f8e457ba0d8b6c71f0515f7f8b361b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
@@ -40,37 +53,33 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
-//Code from last week
-let now = new Date();
-//Date
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let currentDay = days[now.getDay()];
 
-//Time
-let currentHours = now.getHours();
-let currentMinutes = now.getMinutes();
-if (currentHours < 10) {
-  currentHours = `0${currentHours}`;
-}
-if (currentMinutes < 10) {
-  currentMinutes = `0${currentMinutes}`;
-}
+function showWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humid");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#day-and-time");
+  let iconElement = document.querySelector("#icon");
 
-let h2 = document.querySelector("h2");
-h2.innerHTML = `${currentDay}`;
-let h3 = document.querySelector("h3");
-h3.innerHTML = `${currentHours}:${currentMinutes}`;
+  celsiusTemp = response.data.main.temp;
+  temperatureElement.innerHTML = `${Math.round(celsiusTemp)}°`;
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = `${response.data.weather[0].description}`;
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity} %`;
+  windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+  dateElement.innerHTML = getDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
 
 let currentLocation = document.querySelector("#current-location-button");
 currentLocation.addEventListener("click", getCurrentLocation);
 let searchForCity = document.querySelector("#search-form");
 searchForCity.addEventListener("submit", handleSubmit);
+let celsiusTemp = null;
 searchCity("New York");
